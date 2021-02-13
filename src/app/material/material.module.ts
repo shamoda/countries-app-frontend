314eeabdd4333e831e5ee4b8f7a391f33450098b
shamoda <it19161648@my.sliit.lk>
2021-02-13 11:22:55 +0530

    User Interfaces added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+
 
 const MaterialComponents = [
-  MatButtonModule
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatTableModule,
+  MatInputModule
 ]
 
 @NgModule({
-  imports: [ MatButtonModule ],
-  exports: [ MatButtonModule ]
+  imports: [ MaterialComponents ],
+  exports: [ MaterialComponents ]
 })
+
 export class MaterialModule { }
